6163501a2a742a76d92fe701400371e93c055cb8
goldbattle <patfgeneva@gmail.com>
2015-02-28 23:22:36 -0500

    Added - More tuts

--- 0-Cpp-Tutorials/user_input.cpp
+++ 0-Cpp-Tutorials/user_input.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
- 
-// int main() {
-//   std::cout << "Enter a number: ";
-//   int x = 0;
-//   // Read the input from the console
-//   std::cin >> x; // read number from console and store it in x
-//   std:: cout << "You entered " << x << std::endl;
-//   // Return success
-//   return 0;
-// }
+#include <algorithm>
+
+int main() {
+  std::cout << "Enter a number: ";
+  int x;  
+  // Read the input from the console
+  std::cin >> x;
+  std::cout << "You entered " << std::max(x,5) << std::endl;
+  // Return success
+  return 0;
+}
